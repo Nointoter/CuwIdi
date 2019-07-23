e5d8750c14d4167b5580800301415429533a720b
Noin <bmp1999@inbox.ru>
2019-07-23 13:27:58 +0500

    some changes in index.php

--- views/site/index.php
+++ views/site/index.php
@@ -1,21 +1,27 @@
 <?php
 
-foreach ($items as $item)
-{
-    echo '<html> 
-                <body> 
-                    <h2>Idea : ' . $item->ideas_name . '</br> </h2> 
-                </body> 
-               </html>';
-    echo '<html> 
-                <body> 
-                    <h4>Description : ' . $item->info_short . '</br> </h4> 
-                </body> 
-               </html>';
-    echo '<html> 
-                <body> 
-                    <h4>Info : ' . $item->info_long . '</br> </h4> 
-                </body> 
-               </html>';
-}
 ?>
+
+<h1>Пять последних идей</h1>
+<div class="row">
+    <?php
+        foreach ($items as $item)
+        {
+            echo '<html> 
+                    <body> 
+                        <h2>Idea : ' . $item->ideas_name . '</br> </h2> 
+                    </body> 
+                </html>';
+            echo '<html> 
+                    <body> 
+                        <h4>Info : ' . $item->info_short . '</br> </h4> 
+                    </body> 
+                </html>';
+            echo '<html> 
+                    <body> 
+                        <h4>Description : ' . $item->info_long . '</br> </h4> 
+                    </body> 
+                </html>';
+        }
+    ?>
+</div>
