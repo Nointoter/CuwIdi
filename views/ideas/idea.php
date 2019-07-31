66e2debc08779783482d4a886bc675fbf12fe150
Noin <bmp1999@inbox.ru>
2019-07-31 12:39:28 +0500

    some style fix in idea.php

--- views/ideas/idea.php
+++ views/ideas/idea.php
@@ -11,48 +11,66 @@ $form = ActiveForm::begin();
 
 $this->title = 'Просмотр идеи '.strval($model->ideas_name);
 ?>
-
 <div class="row" STYLE="background-color: #FFFFFF; color: #000000">
-    <div class="col-lg-12">
-        <?php
-
-            if (Yii::$app->user->id != $model->creators_id) {
-                echo '<h1>Название : '. Html::encode($model->ideas_name).'</h1>';
-            } else {
-                echo '<h1>' . $form->field($ideasModel, 'ideas_name')->textarea(['autofocus' => true, 'cols' => 1, 'rows' => 1])->label('Название') . '</h1>';
+    <div class="form-group">
+        <div class="col-lg-3">
+            <h3>Название:</h3>
+        </div>
+        <div class="col-lg-9">
+            <?php
+                if (Yii::$app->user->id != $model->creators_id) {
+                    echo '<h3>'. Html::encode($model->ideas_name).'</h3>';
+                } else {
+                    echo '<h3>' . $form->field($ideasModel, 'ideas_name')->textarea(['autofocus' => true, 'cols' => 1, 'rows' => 1])->label(false). '</h3>';
+                }
+            ?>
+        </div>
+    </div>
+</div>
+<div class="row" STYLE="background-color: #FFFFFF; color: #000000">
+    <div class="form-group">
+        <div class="col-lg-3">
+            <h3>Создатель идеи:</h3>
+        </div>
+        <div class="col-lg-3">
+            <h3><a href="/users/profile?id=<?= strval($model->creators_id) ?>" class="" role="button"><?php echo $model->getAuthorsName() ?></a></h3>
+        </div>
+    </div>
+</div>
+<div class="row" STYLE="background-color: #FFFFFF; color: #000000">
+    <div class="form-group">
+        <div class="col-lg-3">
+            <h3>Теги:</h3>
+        </div>
+        <div class="col-lg-9">
+            <?php
+                $array_tags = [];
+                foreach($model->getTags() as $tag) {
+                    $array_tags[] = strval($tag->tag).' ';
+                }
+                implode(", ", $array_tags);
+            ?>
+            <h3><?= implode(", ", $array_tags) ?></h3>
+        </div>
+    </div>
+</div>
+<div class="row" STYLE="background-color: #FFFFFF; color: #000000">
+    <div class="form-group">
+            <div class="col-lg-9 col-lg-offset-3">
+            <?php
+            if (Yii::$app->user->id == $model->creators_id) {
+                echo '<h1>' . $form->field($tagModel, 'tag')->textarea(['autofocus' => true, 'cols' => 1, 'rows' => 1])->label(false) . '</h1>'.
+                    Html::submitButton('Добавить <br> тэг', ['class' => 'btn btn-primary', 'name' => 'add-idea-tag-button']);
             }
-        ?>
+            ?>
+        </div>
     </div>
 </div>
 <div class="row" STYLE="background-color: #FFFFFF; color: #000000">
-    <div class="col-lg-12">
-        <h1>Создатель идеи : <a href="/users/profile?id=<?= strval($model->creators_id) ?>" class="" role="button"><?php echo $model->getAuthorsName() ?></a></h1>
+    <div class="form-group">
+        <p>
+        </p>
     </div>
-</div>
-<div class="row" STYLE="background-color: #FFFFFF; color: #000000">
-    <div class="col-lg-12">
-        <?php
-            $array_tags = [];
-            foreach($model->getTags() as $tag) {
-                $array_tags[] = strval($tag->tag).' ';
-            }
-            implode(", ", $array_tags);
-        ?>
-        <h2>Теги : <?= implode(", ", $array_tags) ?></h2>
-    </div>
-</div>
-<div class="row" STYLE="background-color: #FFFFFF; color: #000000">
-    <div class="col-lg-6">
-        <?php
-        if (Yii::$app->user->id == $model->creators_id) {
-            echo '<h1>' . $form->field($tagModel, 'tag')->textarea(['autofocus' => true, 'cols' => 1, 'rows' => 1])->label(false) . '</h1>'.
-                Html::submitButton('Добавить <br> тэг', ['class' => 'btn btn-primary', 'name' => 'add-idea-tag-button']);
-        }
-        ?>
-    </div>
-</div>
-<div class="row" STYLE="background-color: #FFFFFF; color: #000000">
-    <p></p>
 </div>
 <?php if ($carousel != Null) : ?>
     <div class="row" STYLE="background-color: #FFFFFF; color: #000000">
@@ -79,85 +97,98 @@ $this->title = 'Просмотр идеи '.strval($model->ideas_name);
     </div>
 <?php endif ?>
 <div class="row" STYLE="background-color: #FFFFFF; color: #000000">
-    <div class="col-lg-2">
-        <p></p>
-    </div>
-    <div class="col-lg-2">
-        <?php
-        if (Yii::$app->user->id == $model->creators_id){
-            echo $form->field($image_model, 'imageFile')->fileInput(['autofocus' => true])->label('Добавить изображение');
-            echo Html::submitButton('Добавить', ['class' => 'btn btn-primary', 'name' => 'idea-button']);
-        }
-        ?>
-    </div>
-    <div class="col-lg-2">
-        <p></p>
-    </div>
-    <div class="col-lg-2">
-        <p></p>
-    </div>
-    <div class="col-lg-2">
-        <p>
-        </p>
-        <?php
-        if (Yii::$app->user->id == $model->creators_id && $carousel != Null) {
-            echo '<a href="delete-idea-images?id=' . strval($model->id_ideas) . '" class="btn btn-danger" role="button">Удалить <br> изображения</a>';
-        }
-        ?>
-    </div>
-    <div class="col-lg-2">
-        <p></p>
+    <div class="form-group">
+        <div class="col-lg-3">
+            <h3>Изображения:</h3>
+        </div>
+        <div class="col-lg-2">
+            <?php
+            if (Yii::$app->user->id == $model->creators_id){
+                echo $form->field($image_model, 'imageFile')->fileInput(['autofocus' => true])->label('Добавить изображение');
+                echo Html::submitButton('Добавить', ['class' => 'btn btn-primary', 'name' => 'idea-button']);
+            }
+            ?>
+        </div>
+        <div class="col-lg-2 col-lg-offset-3">
+            <p>
+            </p>
+            <?php
+            if (Yii::$app->user->id == $model->creators_id && $carousel != Null) {
+                echo '<a href="delete-idea-images?id=' . strval($model->id_ideas) . '" class="btn btn-danger" role="button">Удалить <br> изображения</a>';
+            }
+            ?>
+        </div>
     </div>
 </div>
 <div class="row" STYLE="background-color: #FFFFFF; color: #000000">
-    <div class="col-lg-3">
-        <h3>Описание : </h3>
-    </div>
-    <div class="col-lg-9">
-        <?php
-        if (Yii::$app->user->id != $model->creators_id) {
-            echo '<h3><textarea readonly rows="2" cols="65">' . Html::encode($model->info_short) .'</textarea></h3>';
-        } else {
-            echo '<h3>'.$form->field($ideasModel, 'info_short')->textarea(['autofocus' => true, 'rows' => 2, 'cols' => 65])->label(false).'</h3>';
-        }
-        ?>
-    </div>
-</div>
-<div class="row" STYLE="background-color: #FFFFFF; color: #000000">
-    <div class="col-lg-3">
-        <h3>Информация : <br><br><br> </h3>
-        <?php if (Yii::$app->user->id == $model->creators_id) {
-            echo Html::submitButton('Созранить <br> изменения', ['class' => 'btn btn-primary', 'name' => 'change-idea-info-button']);
-        }
-        ?>
-    </div>
-    <div class="col-lg-9">
-        <?php
-        if (Yii::$app->user->id != $model->creators_id) {
-            echo '<h3><textarea readonly rows="10" cols="65">' . Html::encode($model->info_long) . '</textarea></h3>';
-        } else {
-            echo '<h3>'.$form->field($ideasModel, 'info_long')->textarea(['autofocus' => true, 'rows' => 10, 'cols' => 65])->label(false).'</h3>';
-        }
-        ?>
+    <div class="form-group">
+        <div class="col-lg-3">
+            <h3>Описание : </h3>
+        </div>
+        <div class="col-lg-9">
+            <?php
+            if (Yii::$app->user->id != $model->creators_id) {
+                echo '<h3><textarea readonly rows="2" cols="65">' . Html::encode($model->info_short) .'</textarea></h3>';
+            } else {
+                echo '<h3>'.$form->field($ideasModel, 'info_short')->textarea(['autofocus' => true, 'rows' => 2, 'cols' => 65])->label(false).'</h3>';
+            }
+            ?>
+        </div>
     </div>
 </div>
 <div class="row" STYLE="background-color: #FFFFFF; color: #000000">
-    <h1>Комментарии</h1>
-    <?php if (!Yii::$app->user->isGuest) : ?>
-    <div class="col-lg-10">
-        <h3><?= $form->field($commentModel, 'comment')->textarea(['autofocus' => true, 'rows' => 10, 'cols' => 65])->label(false) ?></h3>
+    <div class="form-group">
+        <div class="col-lg-3">
+            <h3>Информация : <br><br><br> </h3>
+            <?php if (Yii::$app->user->id == $model->creators_id) {
+                echo Html::submitButton('Созранить <br> изменения', ['class' => 'btn btn-primary', 'name' => 'change-idea-info-button']);
+            }
+            ?>
+        </div>
+        <div class="col-lg-9">
+            <?php
+            if (Yii::$app->user->id != $model->creators_id) {
+                echo '<h3><textarea readonly rows="10" cols="65">' . Html::encode($model->info_long) . '</textarea></h3>';
+            } else {
+                echo '<h3>'.$form->field($ideasModel, 'info_long')->textarea(['autofocus' => true, 'rows' => 10, 'cols' => 65])->label(false).'</h3>';
+            }
+            ?>
+        </div>
     </div>
-    <div class="col-lg-2">
-        <?php
-            echo '<h1><br></h1>';
-            echo Html::submitButton('Добавить <br> комментарий', ['class' => 'btn btn-primary', 'name' => 'add-ideas-comment-button']);
-        ?>
+</div>
+<div class="row" STYLE="background-color: #FFFFFF; color: #000000">
+    <div class="form-group">
+        <div class="col-lg-3 col-lg-offset-6">
+
+        </div>
     </div>
-    <?php else : ?>
+</div>
+<div class="row" STYLE="background-color: #FFFFFF; color: #000000">
+    <div class="form-group">
+        <div class="col-lg-3">
+            <h3>Комментарии:</h3>
+        </div>
+        <?php if (!Yii::$app->user->isGuest) : ?>
+        <div class="col-lg-9">
+            <h3><?= $form->field($commentModel, 'comment')->textarea(['autofocus' => true, 'rows' => 10, 'cols' => 65])->label(false) ?></h3>
+        </div>
+        <?php else : ?>
         <h3>Для добавления комментариев войдите в аккаунт</h3>
-    <?php endif ?>
-<?php ActiveForm::end() ?>
+        <?php endif ?>
+    </div>
 </div>
+<div class="row" STYLE="background-color: #FFFFFF; color: #000000">
+    <div class="form-group">
+        <div class="col-lg-3 col-lg-offset-9">
+            <?php if (!Yii::$app->user->isGuest) {
+                    echo Html::submitButton('Добавить <br> комментарий', ['class' => 'btn btn-primary', 'name' => 'add-ideas-comment-button']);
+                    echo '<h3><br></h3>';
+                }
+            ?>
+        </div>
+    </div>
+</div>
+<?php ActiveForm::end() ?>
 <?php if ($dataProvider->totalCount > 0) : ?>
 <div class="view-idea-comments">
     <?= GridView::widget([
